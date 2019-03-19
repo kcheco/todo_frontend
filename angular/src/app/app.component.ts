8000d798c32cdc6a2bbdef1fef219c7cd43c344d
Kelvin Checo <checokelvin@gmail.com>
2019-03-18 22:56:25 -0400

    Added Material to project

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { TodoDataService } from './todo-data.service';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'angular';
+  private newTodo: Todo = new Todo({});
+  constructor(private todoDataService: TodoDataService) {
+    this.todoDataService = todoDataService;
+  }
+
+  title = 'TodoAngular';
+
+  addTodo() {
+    this.todoDataService.saveTodo(this.newTodo);
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getTodos();
+  }
 }
