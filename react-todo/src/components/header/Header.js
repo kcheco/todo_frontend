cebdfcce2a71d9fee8efae4d8b31af1463aee308
Kelvin Checo <checokelvin@gmail.com>
2019-03-26 01:49:51 -0400

    defining app folder structure and worked on rendering updated header component

---
+++ react-todo/src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <a className="App-link" href="#">
+          { this.props.appName }
+        </a>
+      </header>
+    );
+  }
+}
+
+export default Header;
